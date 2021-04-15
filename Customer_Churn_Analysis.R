1f6e22394d75b5dde1dc5b6509fd5ee645fc81bb
monicafng <monicafkm@hotmail.com>
2021-04-14 22:26:41 -0700

    10 k-folds

--- Customer_Churn_Analysis.R
+++ Customer_Churn_Analysis.R
@@ -1,153 +1,132 @@
+# Import libraries
+library(psych)
+library(tidyverse)
+library(ggplot2)
+
+# Load the data
 telco <- read.csv("Telco-Customer-Churn.csv")
 
+######## DATA EXPLORATION ########
 View(telco)
 str(telco)
 summary(telco)
 
-library(ggplot2)
+# Descriptive statistics for continuous numerical variables
+telco %>% select(tenure, MonthlyCharges, TotalCharges) %>% describe()
 
-ggplot(telco, aes(gender)) + geom_bar(aes(fill = Churn))
+# Histogram for tenure
+ggplot(telco, aes(x=tenure)) + geom_histogram(binwidth=0.8, aes(fill=..count..))
 
-ggplot(telco, aes(tenure, TotalCharges)) + geom_point(aes(color=Churn))
+# Compare churn rate for female vs. male
+ggplot(telco, aes(gender)) + geom_bar(aes(fill=Churn), position='dodge')
 
-telco$Male <- ifelse(telco$gender == 'Male', 1, 0)
-telco$Male <- factor(telco$Male)
-telco$SeniorCitizen <- factor(telco$SeniorCitizen)
-telco$Partner <- ifelse(telco$Partner == 'Yes', 1, 0)
-telco$Partner <- factor(telco$Partner)
-telco$Dependents <- ifelse(telco$Dependents == 'Yes', 1, 0)
-telco$Dependents <- factor(telco$Dependents)
-telco$PaperlessBilling <- ifelse(telco$PaperlessBilling == 'Yes', 1, 0)
-telco$PaperlessBilling <- factor(telco$PaperlessBilling)
-telco$PhoneService <- ifelse(telco$PhoneService == 'Yes', 1, 0)                                                        
-telco$PhoneService <- factor(telco$PhoneService)
-telco$Churn <- ifelse(telco$Churn == 'Yes', 1, 0)
-telco$Churn <- factor(telco$Churn)
+# Boxplot for tenure vs. Churn
+ggplot(telco, aes(x=factor(Churn), y=tenure)) + geom_boxplot(aes(fill=factor(Churn)))
 
+# Boxplot for MonthlyCharges vs. Churn
+ggplot(telco, aes(x=factor(Churn), y=MonthlyCharges)) + geom_boxplot(aes(fill=factor(Churn)))
 
-categorical <- c('MultipleLines','InternetService','OnlineSecurity','OnlineBackup',
-                 'DeviceProtection','TechSupport','StreamingTV','StreamingMovies',
-                 'Contract', 'PaymentMethod')
-telco[,categorical] <- lapply(telco[,categorical], factor)
+# Boxplot for TotalCharges vs. Churn
+ggplot(telco, aes(x=factor(Churn), y=TotalCharges)) + geom_boxplot(aes(fill=factor(Churn)))
 
-table(telco$InternetService)
-table(telco$MultipleLines)
-table(telco$OnlineSecurity)
-table(telco$DeviceProtection)
-table(telco$TechSupport)
-table(telco$StreamingTV)
-table(telco$StreamingMovies)
-
-ggplot(telco, aes(x=TotalCharges)) + geom_histogram(aes(fill=Churn, bins = 50),
-                                                    position = 'dodge')
-
-str(telco)
-
-library(dplyr)
-
-df <- telco %>% select(customerID, Male, SeniorCitizen, Partner, Dependents,
-                       tenure, PhoneService, MultipleLines, InternetService,
-                       OnlineSecurity, OnlineBackup, DeviceProtection, TechSupport,
-                       StreamingTV, StreamingMovies, Contract, PaperlessBilling,
-                       PaymentMethod, MonthlyCharges, TotalCharges, Churn)
-
-str(df)
-
+######## DATA PREPROCESSING ########
 # Check missing values
 library(Amelia)
-missmap(df, y.at = c(1), y.labels = c(''),
+missmap(telco, y.at = c(1), y.labels = c(''),
         col = c('yellow','black'))
-sum(is.na(df))
-str(df)
+sum(is.na(telco))
+str(telco)
 
 # Omit NA data (only 0.1%)
-df <- na.omit(df)
-str(df)
+telco <- na.omit(telco)
+str(telco)
 
-library(psych)
-describe(telco)
+# Convert variables to dummies
+telco$Male <- ifelse(telco$gender == 'Male', 1, 0)
+telco$Partner <- ifelse(telco$Partner == 'Yes', 1, 0)
+telco$Dependents <- ifelse(telco$Dependents == 'Yes', 1, 0)
+# if customer has one-year contract -> Contract1yr == 1
+# if customer has two-year contract -> Contract2yr == 1
+# else customer has month-to-month contract
+telco$Contract1yr <- ifelse(telco$Contract == 'One year', 1, 0)
+telco$Contract2yr <- ifelse(telco$Contract == 'Two year', 1, 0)
+telco$PaperlessBilling <- ifelse(telco$PaperlessBilling == 'Yes', 1, 0)
+# for payment method:
+# 'Electronic check' and 'Mailed check' -> Check
+# 'Bank transfer' and 'Credit card' -> Automatic payment
+# hence, AutoPayment -> if customer uses automatic payment then '1' else '0' for check payment
+telco$AutoPayment <- ifelse(telco$PaymentMethod %in% c('Electronic check', 'Mailed check'), 0, 1)
+telco$PhoneService <- ifelse(telco$PhoneService == 'Yes', 1, 0) 
+telco$MultipleLines <- ifelse(telco$MultipleLines == 'Yes', 1, 0)
+telco$DSL <- ifelse(telco$InternetService == 'DSL', 1, 0)
+telco$FiberOptic <- ifelse(telco$InternetService == 'Fiber optic', 1, 0)
+telco$InternetService <- ifelse(telco$InternetService == 'No', 0, 1)
+telco$OnlineSecurity <- ifelse(telco$OnlineSecurity == 'Yes', 1, 0)
+telco$OnlineBackup <- ifelse(telco$OnlineBackup == 'Yes', 1, 0)
+telco$DeviceProtection <- ifelse(telco$DeviceProtection == 'Yes', 1, 0)
+telco$TechSupport <- ifelse(telco$TechSupport == 'Yes', 1, 0)
+telco$StreamingTV <- ifelse(telco$StreamingTV == 'Yes', 1, 0)
+telco$StreamingMovies <- ifelse(telco$StreamingMovies == 'Yes', 1, 0)
+telco$Churn <- ifelse(telco$Churn == 'Yes', 1, 0)
 
+categorical.variables <- c('SeniorCitizen','Partner','Dependents','PhoneService',
+                           'MultipleLines','InternetService','OnlineSecurity',
+                           'OnlineBackup','DeviceProtection','TechSupport',
+                           'StreamingTV','StreamingMovies','PaperlessBilling',
+                           'Churn','Male','Contract1yr','Contract2yr',
+                           'AutoPayment','DSL','FiberOptic')
+telco[,categorical.variables] <- lapply(telco[,categorical.variables], factor)
 
-
-# Creating New Columns
-df$MultipleLines <- ifelse(df$MultipleLines == 'Yes', 1, 0)
-df$MultipleLines <- factor(df$MultipleLines)
-df$DSL <- ifelse(df$InternetService == 'DSL', 1, 0)
-df$DSL <- factor(df$DSL)
-df$FiberOptic <- ifelse(df$InternetService == 'Fiber optic', 1, 0)
-df$FiberOptic <- factor(df$FiberOptic)
-df$InternetService <- ifelse(df$InternetService == 'No', 0, 1)
-df$InternetService <- factor(df$InternetService)
-df$OnlineSecurity <- ifelse(df$OnlineSecurity == 'Yes', 1, 0)
-df$OnlineSecurity <- factor(df$OnlineSecurity)
-df$OnlineBackup <- ifelse(df$OnlineBackup == 'Yes', 1, 0)
-df$OnlineBackup <- factor(df$OnlineBackup)
-df$DeviceProtection <- ifelse(df$DeviceProtection == 'Yes', 1, 0)
-df$DeviceProtection <- factor(df$DeviceProtection)
-df$TechSupport <- ifelse(df$TechSupport == 'Yes', 1, 0)
-df$TechSupport <- factor(df$TechSupport)
-df$StreamingTV <- ifelse(df$StreamingTV == 'Yes', 1, 0)
-df$StreamingTV <- factor(df$StreamingTV)
-df$StreamingMovies <- ifelse(df$StreamingMovies == 'Yes', 1, 0)
-df$StreamingMovies <- factor(df$StreamingMovies)
-
-df$Contract1yr <- ifelse(df$Contract == 'One year', 1, 0)
-df$Contract1yr <- factor(df$Contract1yr)
-df$Contract2yr <- ifelse(df$Contract == 'Two year', 1, 0)
-df$Contract2yr <- factor(df$Contract2yr)
-df$AutoPayment <- ifelse(df$PaymentMethod %in% c('Electronic check', 'Mailed check'), 0, 1)
-                           
-                           
-
-str(df)
-colnames(df)
-
-df <- df %>% select(Male, SeniorCitizen, Partner,
-                    Dependents, tenure, Contract1yr, Contract2yr,
-                    PaperlessBilling, AutoPayment, MonthlyCharges,
-                    TotalCharges, PhoneService, MultipleLines,
-                    InternetService, DSL, FiberOptic, OnlineSecurity,
-                    OnlineBackup, DeviceProtection, TechSupport,
-                    StreamingTV, StreamingMovies, Churn)
-
-df$techaddons <- ifelse(df$OnlineSecurity == 1|df$OnlineBackup== 1 | 
-                             df$DeviceProtection== 1 |df$TechSupport== 1,1,0)
-df$streamingaddon <- ifelse(df$StreamingTV==1|df$StreamingMovies == 1,1,0)
-
-df <- df %>% select(Male, SeniorCitizen, Partner,
-                    Dependents, tenure, Contract1yr, Contract2yr,
-                    PaperlessBilling, AutoPayment, MonthlyCharges,
-                    TotalCharges, PhoneService, MultipleLines,
-                    InternetService, DSL, FiberOptic, techaddons,
-                    streamingaddon, Churn)
+df <- telco %>% select(Male, SeniorCitizen, Partner, Dependents,
+                       tenure, PhoneService, MultipleLines, InternetService,
+                       DSL, FiberOptic, OnlineSecurity, OnlineBackup, 
+                       DeviceProtection, TechSupport, StreamingTV, StreamingMovies,
+                       Contract1yr, Contract2yr, PaperlessBilling, AutoPayment,
+                       MonthlyCharges, TotalCharges, Churn)
 
 str(df)
 
-df$AutoPayment <- factor(df$AutoPayment)
-df$techaddons <- factor(df$techaddons)
-df$streamingaddon <- factor(df$streamingaddon)
+######## DATA AND DIMENSION REDUCTION ########
 
-str(df)
 
-# Building a model
-library(caTools)
+######## PARTITION THE DATA ########
+# train test split
+library(caret)
+inTrain <- createDataPartition(y=df$Churn, p=0.75, list=FALSE)
+df.train <- df[inTrain,]
+df.test <- df[-inTrain,]
 
-set.seed(101)
-split <- sample.split(df$Churn, SplitRatio = 0.7)
-train <- subset(df, split == TRUE)
-test <- subset(df, split == FALSE)
+prop.table(table(df$Churn)) # imbalanced classification
 
-model <- glm(Churn ~ .,
-             family = binomial(link="logit"),
-             data = train[, -1])
+#library(ROSE) # synthetic data generation
+#df.rose <- ROSE(Churn ~ ., data = df.train, seed = 1)$data
+#table(df.rose$Churn)
+#prop.table(table(df.rose$Churn))
 
+# k-fold cross validation
+train.control <- trainControl(method="cv", number=10, savePredictions=TRUE)
+# train the model
+model <- train(Churn ~ ., data=df.train, method="glm", trControl=train.control)
+print(model)
+
+pred <- model$pred
+pred$equal <- ifelse(pred$pred == pred$obs, 1, 0)
+
+eachfold <- pred %>% 
+  group_by(Resample) %>% 
+  summarise_at(vars(equal),
+               list(Accuracy=mean))
+
+eachfold
+ggplot(data=eachfold, aes(x=Resample, y=Accuracy, group=1)) + geom_boxplot(color="maroon") + geom_point()
+
+# get prediction on test.df
+model <- glm(Churn ~ ., family="binomial", data = df.train)
 summary(model)
 
-test$predictedChurn <- predict(model, newdata = test[, -19], type = 'response')
-test$predictedChurn <- ifelse(test$predictedChurn > 0.5, 1, 0)
-table(test$Churn, test$predictedChurn)
-
-print(paste('Accuracy =',round(((1398+303)/(1398+151+258+303)), 2)))
+test.pred <- predict(model, newdata=df.test[, -23], type='response')
+test.pred <- ifelse(test.pred > 0.5, 1, 0)
+table(df.test$Churn, test.pred)
 
 
 
